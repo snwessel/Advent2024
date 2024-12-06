0b182eb4352554315438884d5ca68f6f7bfce623
Sarah Wessel <swessel@broadinstitute.org>
2024-12-05 19:51:45 -0500

    day 5 part 2 solution

--- advent_2024/day5.py
+++ advent_2024/day5.py
@@ -1,3 +1,4 @@
+from functools import cmp_to_key
 
 filepath = "advent_2024/inputs/day5.txt"
 # filepath = "advent_2024/test_inputs/day5.txt" # for testing
@@ -42,15 +43,39 @@ def part1():
 
     total_sum = 0
     for update in updates:
-        update_is_valid = is_update_valid(update, rules)
-        if update_is_valid:
+        if is_update_valid(update, rules):
             print("VALID UPDATE", update)
             middle_index = len(update) // 2 # integer division rounds down
             total_sum += int(update[middle_index])
     return total_sum
 
+
 def part2():
-    pass # TODO:
+    # Possible solutions for re-ordering updates:
+    # - Option 1: construct a graph of the rules and do BFS (downside: it might end up being multiple graphs)
+    # - Option 2: re-order each update by applying the rules until it eventually passes all rules (simple but possibly very slow)
+    # - Option 3: (selected) make a custom sort comparitor that iterates through the rules
+    rules, updates = get_input_rules_and_updates()
+
+    def compare_items(val1, val2):
+        """Used to override the comparator in python's built in sort method"""
+        for rule_val1, rule_val2 in rules:
+            if val1 == rule_val1 and val2 == rule_val2:
+                return -1
+            elif val1 == rule_val2 and val2 == rule_val1:
+                return 1
+        return 0
+
+
+    total_sum = 0
+    for update in updates:
+        if not is_update_valid(update, rules):
+            print("INVALID UPDATE", update)
+            update.sort(key=cmp_to_key(compare_items))
+            print("sorted to:", update)
+            middle_index = len(update) // 2 # integer division rounds down
+            total_sum += int(update[middle_index])
+    return total_sum
 
 print("Part 1:", part1())
 print("Part 2:", part2())
